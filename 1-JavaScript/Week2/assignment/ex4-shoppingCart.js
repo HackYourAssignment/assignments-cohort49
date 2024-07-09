9f3026f02905e0ba949f3a6e6c6b572a60dfba57
Nima Saghikalhori <nimasaghikalhori@gmail.com>
2024-07-09 20:34:07 +0200

    ex4 is done

--- 1-JavaScript/Week2/assignment/ex4-shoppingCart.js
+++ 1-JavaScript/Week2/assignment/ex4-shoppingCart.js
@@ -23,7 +23,7 @@ you have more than 3 items in your shopping cart the first item gets taken out.
 const shoppingCart = ['bananas', 'milk'];
 
 // ! Function to be tested
-function addToShoppingCart(/* parameters go here */item) {
+function addToShoppingCart(item) {
   // TODO complete this function
 
   shoppingCart.push(item);
