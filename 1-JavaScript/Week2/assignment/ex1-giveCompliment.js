449af0e45f4ab0f36ad0ca3ade1f168a43afc1f6
Alexander Vasyliev <saddon11@gmail.com>
2024-07-10 19:22:23 +0200

    do js  week2 assignment

--- 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
+++ 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
@@ -1,30 +1,26 @@
 'use strict';
-/* -----------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-1-you-are-amazing
+function giveCompliment(name) {
+  const compliments = [
+    'great',
+    'awesome',
+    'fantastic',
+    'amazing',
+    'wonderful',
+    'outstanding',
+    'brilliant',
+    'excellent',
+    'superb',
+    'marvelous',
+  ];
 
-1. Complete the function named `giveCompliment`as follows:
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  const compliment = compliments[randomIndex];
 
-   - It should take a single parameter: `name`.
-   - Its function body should include a variable that holds an array,
-     `compliments`, initialized with 10 strings. Each string should be a
-     compliment, like `"great"`, `"awesome"` and so on.
-   - It should randomly select a compliment from the array.
-   - It should return the string "You are `compliment`, `name`!", where
-     `compliment` is a randomly selected compliment and `name` is the name that
-     was passed as an argument to the function.
-
-2. Call the function three times, giving each function call the same argument:
-   your name.
-   Use `console.log` each time to display the return value of the
-   `giveCompliment` function to the console.
------------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+  return `You are ${compliment}, ${name}!`;
 }
 
 function main() {
-  // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'Oleksandr';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
--- 1-JavaScript/Week2/assignment/ex6-totalCost.js
+++ 1-JavaScript/Week2/assignment/ex6-totalCost.js
@@ -1,42 +1,31 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week3#exercise-6-total-cost-is
-
-You want to buy a couple of things from the supermarket to prepare for a party.
-After scanning all the items the cashier wants to give you the total price, but
-the machine is broken! Let's write her a function that does it for her
-instead!
-
-1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
-
-2. Complete the function called `calculateTotalPrice`.
-
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
-
-3. Complete the unit test functions and verify that all is working as expected.
------------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  beers: 1.75,
+  chips: 0.99,
+  soda: 1.5,
+  pizza: 4.99,
+  cake: 5.5,
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(cart) {
+  let total = 0;
+  for (const item of Object.keys(cart)) {
+    total += cart[item];
+  }
+  return `Total: €${total.toFixed(2)}`;
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  console.assert(calculateTotalPrice.length === 1);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  const expected = 'Total: €14.73';
+  const actual = calculateTotalPrice(cartForParty);
+  console.assert(actual === expected);
 }
 
 function test() {
