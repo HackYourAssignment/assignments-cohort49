ff933fae0ca6443bd343f5a7ebe210d539924622
Nima Saghikalhori <nimasaghikalhori@gmail.com>
2024-07-10 11:56:40 +0200

    ex6 is done

--- 1-JavaScript/Week2/assignment/ex6-totalCost.js
+++ 1-JavaScript/Week2/assignment/ex6-totalCost.js
@@ -8,35 +8,52 @@ the machine is broken! Let's write her a function that does it for her
 instead!
 
 1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
+  should be a grocery item (like `beers` or `chips`) and hold a number value
+  (like `1.75` or `0.99`).
 
 2. Complete the function called `calculateTotalPrice`.
 
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
+  - It takes one parameter: an object that contains properties that only contain
+    number values.
+  - Loop through the object and add all the number values together.
+  - Return a string: "Total: €`amount`".
 
 3. Complete the unit test functions and verify that all is working as expected.
 -----------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+
+  beer : 1.50,
+  bread : 2,
+  sauce : 1.20,
+  egg : 6.50,
+  milk : 1.80
+
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(shoppingCart) {
+  
+  let total = 0;
+  Object.keys(shoppingCart).forEach(key => {
+    total += shoppingCart[key];
+  });
+  
+  return `total : €${total.toFixed(2)}`;
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  
+  console.assert(calculateTotalPrice.length === 1, 'calculateTotalPrice should take one parameter');
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  
+  const expected = 'total : €13.00';
+  const actual = calculateTotalPrice(cartForParty);
+
+  console.assert(actual === expected, `Expected "${expected}", but got "${actual}"`);
 }
 
 function test() {
