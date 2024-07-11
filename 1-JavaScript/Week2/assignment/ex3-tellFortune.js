c5f3ec36c803b1ff49238479acf1d94da8f552fa
fatemeh alinejad <fatemeh.alinejad.dev@gmail.com>
2024-07-11 21:02:06 +0200

    shoppingCart assignment

--- 1-JavaScript/Week2/assignment/ex3-tellFortune.js
+++ 1-JavaScript/Week2/assignment/ex3-tellFortune.js
@@ -1,43 +1,11 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week3#exercise-3-be-your-own-fortune-teller
-
-Why pay a fortune teller when you can just program your fortune yourself?
-
-1. Create four arrays, `numKids`, `partnerNames`, `locations` and `jobTitles`. 
-   Give each array five random values that have to do with the name of 
-   the variable.
-
-2. Complete the function `selectRandomly`. This function should take an array 
-   as a parameter and return a randomly selected element as its return value.
-
-3. Complete the function named `tellFortune` as follows:
-
-   - It should take four arguments (in the order listed): 
-     * the array with the options for the number of children, 
-     * the array with the options for the partner's name, 
-     * the array with the options for the geographic location and 
-     * the array with the options for the job title.
-   - It should use the `selectRandomly` function to randomly select values from 
-     the arrays.
-   - It should return a string: "You will be a `jobTitle` in `location`, 
-    married to `partnerName` with `numKids` kids."
-
-4. Call the function three times, passing the arrays as arguments. Use `
-   console.log` to display the results.
-
-Note: The DRY principle is put into practice here: instead of repeating the code to 
-randomly select array elements four times inside the `tellFortune` function 
-body, this code is now written once only in a separated function.
------------------------------------------------------------------------------*/
 
 const numKids = ["1","2", "3","4", "5"];
 const partnerNames = ["Micheal", "Nick", "Flora", "Saskia", "Isabelle"];
 const locations = ["Los Angeles", "Cairo", "Tokyo", "Amsterdam", "Tehran"];
 const jobTitles = ["Software Engineer", "Nurse", "Artist", "Athlete", "Designer"];
 
-// This function should take an array as its parameter and return
-// a randomly selected element as its return value.
+
 function selectRandomly(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
