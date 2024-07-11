ebbcdb50a49e8c6f94ce61a022923c295d598ac1
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-07-11 23:39:09 +0200

    exercise JavaScript-2

--- 1-JavaScript/Week2/assignment/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week2/assignment/ex5-shoppingCartPure.js
@@ -16,9 +16,16 @@ it pure. Do the following:
 5. Confirm that you function passes the provided unit tests.
 ------------------------------------------------------------------------------*/
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
-  // TODO complete this function
-}
+function addToShoppingCart(shoppingCart, groceryItem) {
+  const newCart = [...shoppingCart];
+  
+  if (newCart.length < 3) {
+    newCart.push(groceryItem);
+  } else {
+    newCart[0] = groceryItem;
+  }
+  
+  return newCart;}
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
