2544b59f77785c5e5e9ecd57535ff629bfbabfa8
Sofiia <sonia.nosenko@gmail.com>
2024-07-12 18:59:45 +0200

    done task 4, 5, test task 4, 5

--- 1-JavaScript/Week3/assignment/ex4-observable/ex4-observable.js
+++ 1-JavaScript/Week3/assignment/ex4-observable/ex4-observable.js
@@ -17,10 +17,12 @@ function createObservable() {
   const subscribers = [];
   return {
     subscribe: function (subscriber) {
-      // TODO complete this function
+      subscribers.push(subscriber);
     },
     notify: function (message) {
-      // TODO complete this function
+      subscribers.forEach((subscriber) => {
+        subscriber(message);
+      });
     },
   };
 }
