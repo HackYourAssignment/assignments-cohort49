ef193fcebdd85f83037b541385db3aae205a7d3a
Tugbanur <tnur.yurtsever@gmail.com>
2024-07-14 12:10:38 +0200

    modified

--- 1-JavaScript/Week3/assignment/ex1-doubleEvenNumbers.test.js
+++ 1-JavaScript/Week3/assignment/ex1-doubleEvenNumbers.test.js
@@ -12,18 +12,15 @@ Let's rewrite it (or _refactor_ it, as experienced developers would call it):
 // ! Function to be tested
 function doubleEvenNumbers(numbers) {
   // TODO rewrite the function body using `map` and `filter`.
-  const newNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      newNumbers.push(numbers[i] * 2);
-    }
-  }
-  return newNumbers;
+  return numbers 
+  .filter(number => number % 2 === 0)
+  .map(number => number * 2)
+  
 }
 
-// ! Unit test (using Jest)
+// // ! Unit test (using Jest)
 test('doubleEvenNumbers should take the even numbers and double them', () => {
-  const actual = doubleEvenNumbers([1, 2, 3, 4]);
+    const actual = doubleEvenNumbers([1, 2, 3, 4]);
   const expected = [4, 8];
   expect(actual).toEqual(expected);
-});
+ });
