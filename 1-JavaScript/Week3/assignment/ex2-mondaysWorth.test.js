a99eff0c2c6bb80335ef20752c256cee47076b5e
fadysaadeddin <fadysaadeddin@gmail.com>
2024-07-18 03:05:43 +0200

    assignment week3

--- 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
+++ 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
@@ -31,8 +31,12 @@ const mondayTasks = [
 
 const hourlyRate = 25;
 
-function computeEarnings(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function computeEarnings(tasks, hourlyRate) {
+  const computerEarning = tasks
+    .map((task) => (task.duration / 60) * hourlyRate)
+    .reduce((sum, earning) => sum + earning, 0);
+
+  return `€${(Math.round(computerEarning * 100) / 100).toFixed(2)}`;
 }
 
 // ! Unit tests (using Jest)
