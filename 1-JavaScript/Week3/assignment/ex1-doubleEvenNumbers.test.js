fc7051023261d8149424debc0e2f05ede33307d3
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-07-18 23:43:40 +0200

    Elias adding homework week-3

--- 1-JavaScript/Week3/assignment/ex1-doubleEvenNumbers.test.js
+++ 1-JavaScript/Week3/assignment/ex1-doubleEvenNumbers.test.js
@@ -12,13 +12,17 @@ Let's rewrite it (or _refactor_ it, as experienced developers would call it):
 // ! Function to be tested
 function doubleEvenNumbers(numbers) {
   // TODO rewrite the function body using `map` and `filter`.
-  const newNumbers = [];
+  return numbers
+    .filter((number) => number % 2 === 0)
+    .map((number) => number * 2);
+
+  /* const newNumbers = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 === 0) {
       newNumbers.push(numbers[i] * 2);
     }
   }
-  return newNumbers;
+  return newNumbers; */
 }
 
 // ! Unit test (using Jest)
