152a57296ad4b6b8af53f27b282232ee60679e7e
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-08-08 10:52:37 +0200

    prettier fix

--- 2-Browsers/Week1/assignment/ex3-hijackLogo.js
+++ 2-Browsers/Week1/assignment/ex3-hijackLogo.js
@@ -1,15 +1,12 @@
 'use strict';
 
 function hijackGoogleLogo() {
-  // Select all elements that might contain the Google logo
   const googleLogos = document.querySelectorAll('img[alt="Google"]');
 
-  // Loop through each logo and replace its src and srcset attributes
   googleLogos.forEach((logo) => {
     logo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
     logo.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
   });
 }
 
-// Execute the function to replace the logos
 hijackGoogleLogo();
