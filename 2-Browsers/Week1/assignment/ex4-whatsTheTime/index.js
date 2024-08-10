ee492817c4a42e3561d2f65f11182d0bce06afa9
Tugbanur <tnur.yurtsever@gmail.com>
2024-08-10 20:00:55 +0200

    modified

--- 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/assignment/ex4-whatsTheTime/index.js
@@ -8,7 +8,18 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
+  const timeElement = document.getElementById('current-time');
+  const now = new Date();
+
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+
+  timeElement.textContent = currentTime;
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+setInterval(addCurrentTime, 1000);
+
+window.addEventListener('load', addCurrentTime);
