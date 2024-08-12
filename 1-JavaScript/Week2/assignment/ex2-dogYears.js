de533c0025c68c36434a7be82ccee5b434dcb767
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-08-12 19:33:12 +0200

    undo exercise JavaScript-2

--- 1-JavaScript/Week2/assignment/ex2-dogYears.js
+++ 1-JavaScript/Week2/assignment/ex2-dogYears.js
@@ -16,9 +16,9 @@ calculate it!
    ages.
 -----------------------------------------------------------------------------*/
 
-function calculateDogAge(/* TODO parameter(s) go here */) {
-  // TODO complete this function
-}
+function calculateDogAge(humanAge) {
+  const dogAge = humanAge * 7;
+  return `Your doggie is ${dogAge} years old in dog years!`;}
 
 function main() {
   console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
