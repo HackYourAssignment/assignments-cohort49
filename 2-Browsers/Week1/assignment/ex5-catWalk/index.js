260e1e9872d6a72e1d8b5c7675c73cfd04b42b46
Nima <nimasaghi.dev@gmail.com>
2024-08-31 18:04:26 +0200

    Update index.js
    second modification.

--- 2-Browsers/Week1/assignment/ex5-catWalk/index.js
+++ 2-Browsers/Week1/assignment/ex5-catWalk/index.js
@@ -1,58 +1,30 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
+const cat = document.querySelector('img');
 
-1. Create a variable to store a reference to the `<img>` element.
-2. Change the style of the `<img>` to have a `left` of `0px`, so that it starts 
-   at the left hand of the screen.
-3. Complete the function called catWalk() to move the cat 10 pixels to the right
-   of where it started, by changing the `left` style property.
-4. Call that function every 50 milliseconds. Your cat should now be moving 
-   across the screen from left to right. Hurrah!
-5. When the cat reaches the right-hand of the screen, restart them at the left 
-   hand side (`0px`). So they should keep walking from left to right across the 
-   screen, forever and ever.
-6. When the cat reaches the middle of the screen, replace the img with an image 
-   of a cat dancing (use this URL given below), keep it dancing for 5 seconds, 
-   and then replace the img with the original image and have it 
-   continue the walk.
+cat.style.left = '0px';
 
-   Dancing cat URL:
+const originalCatSrc = cat.src;
+const dancingCatSrc = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+let isDancing = false;
 
-   https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
------------------------------------------------------------------------------*/
 function catWalk() {
-  const img = document.querySelector('img');
-  let left = 0;
-  const screenWidth = window.innerWidth;
-  const middleScreenWidth = screenWidth / 2;
+  if (isDancing) return;
 
-  function moveCat() {
-    left += 10;
-    img.style.left = `${left}px`;
+  let currentLeft = parseInt(cat.style.left);
+  cat.style.left = (currentLeft + 10) + 'px';
 
-    if (
-      left >= middleScreenWidth - img.width / 2 &&
-      left <= middleScreenWidth + img.width / 2
-    ) {
-      img.src =
-        "https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif";
-
-      setTimeout(() => {
-        img.src = "http://www.animation.com/clip_art/images/cat-walk.gif";
-      }, 5000);
-    }
-
-    if (left >= screenWidth) {
-      left = 0;
-    }
+  if (currentLeft > window.innerWidth - cat.width) {
+    cat.style.left = '0px';
   }
 
-  setInterval(moveCat, 50);
+  if (currentLeft > (window.innerWidth - cat.width) / 2 && currentLeft < (window.innerWidth - cat.width) / 2 + 10) {
+    isDancing = true;
+    cat.src = dancingCatSrc;
+    setTimeout(() => {
+      cat.src = originalCatSrc;
+      isDancing = false;
+    }, 5000);
+  }
 }
 
-// Run the catWalk function when the page loads
-window.addEventListener("load", catWalk);
-
-
-
+setInterval(catWalk, 50);
