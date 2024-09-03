7d2deb4f2e3f26d58e91372a552a29d657da7cf4
Alexander Vasyliev <saddon11@gmail.com>
2024-09-03 17:35:44 +0200

    done: ex1-4

--- 3-UsingAPIs/Week1/assignment/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/assignment/ex2-checkDoubleDigits.js
@@ -1,19 +1,13 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/3-UsingAPIs/Week1#exercise-2-is-it-a-double-digit-number
 
-Complete the function called `checkDoubleDigits` such that:
-
-- It takes one argument: a number
-- It returns a `new Promise`.
-- If the number between 10 and 99 it should resolve to the string
-  "This is a double digit number!".
-- For any other number it should reject with an an Error object containing: 
-  "Expected a double digit number but got `number`", where `number` is the 
-  number that was passed as an argument.
-------------------------------------------------------------------------------*/
-function checkDoubleDigits(/* TODO add parameter(s) here */) {
-  // TODO complete this function
+function checkDoubleDigits(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 10 && number <= 99) {
+      resolve('This is a double digit number!');
+    } else {
+      reject(new Error(`Expected a double digit number but got ${number}`));
+    }
+  });
 }
 
 function main() {
--- 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
@@ -1,35 +1,12 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/3-UsingAPIs/Week1#exercise-4-throw-the-dice-for-a-poker-dice-game
-
-For this exercise you should do the following:
-  - Refactor the `rollDice()` function to throw five dice in one go, by 
-    using `.map()` on the `dice` array to create an array of promises for use 
-    with `Promise.all()`.
-  - A successful (i.e. resolved) throw should output a message similar to: 
-      Resolved! [ 'JACK', 'QUEEN', 'QUEEN', 'NINE', 'JACK' ]
-  - An unsuccessful (i.e. rejected) throw should output a message similar to:
-      Rejected! Die 3 rolled off the table.
-
-The provided rollDie() function logs the value of a die as it rolls, 
-time-stamped with the time of day (with millisecond accuracy) to the console. 
-Once you have successfully completed this exercise you will notice that the 
-intermediate messages are output in bursts of up to five at a time as the dice 
-finish rolling asynchronously.
-
-You may also notice that, in the case of a rejected promise, dice that have not
-yet finished their roll continue to do so. 
-Can you explain why? Please add your answer as a comment to the end of the 
-exercise file.
-------------------------------------------------------------------------------*/
-
 // The line below makes the rollDie() function available to this file.
 // Do not change or remove it.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const dicePromises = dice.map((die) => rollDie(die));
+
+  return Promise.all(dicePromises);
 }
 
 function main() {
@@ -43,3 +20,6 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+// every promise in promise.all() is running individually and asynchronously
+// if one of the promises in promise.all() is rejected, it doesn't stop the execution of the other promises in promise.all()
