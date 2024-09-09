58ef014bc8903a93abc9cc4797bb0c5135e6c89e
bteicho@gmail.com <btegicho@gmail.com>
2024-09-09 16:49:38 +0200

    added missing README

--- 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
@@ -1,3 +1,14 @@
+'use strict';
+/*------------------------------------------------------------------------------
+Full description at: https://github.com/HackYourFuture/Assignments/tree/main/3-UsingAPIs/Week1#exercise-1-john-who
+
+Rewrite this function, but replace the callback syntax with the Promise syntax:
+- Have the `getAnonName` function return a `new Promise`.
+- If the Promise `resolves`, pass the full name as an argument to resolve with.
+- If the Promise `rejects`, pass an error as the argument to reject with: "You 
+  didn't pass in a first name!"
+------------------------------------------------------------------------------*/
+
 const getAnonName = (firstName) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
