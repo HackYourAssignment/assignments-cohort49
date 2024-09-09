4fdf431e544f3b9fcfe7dbbc2102e64f063e4fa9
Alexander Vasyliev <saddon11@gmail.com>
2024-09-09 14:53:02 +0200

    fix ex1

--- 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
@@ -2,16 +2,24 @@
 
 const getAnonName = (firstName) => {
   return new Promise((resolve, reject) => {
-    if (firstName) {
-      resolve(firstName + ' Doe');
-    } else {
-      reject(new Error("You didn't pass in a first name!"));
-    }
+    setTimeout(() => {
+      if (firstName) {
+        resolve(firstName + ' Doe');
+      } else {
+        reject(new Error("You didn't pass in a first name!"));
+      }
+    }, 1000);
   });
 };
 
 function main() {
-  getAnonName('John');
+  getAnonName('John')
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 // ! Do not change or remove the code below
