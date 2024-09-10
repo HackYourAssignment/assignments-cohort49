ee7c83b0e0aa0a7fcd1efd578844facc3fa252d2
bteicho@gmail.com <btegicho@gmail.com>
2024-09-10 09:38:05 +0200

    assignment-ex1-completed

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -19,27 +19,46 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
 ------------------------------------------------------------------------------*/
 function requestData(url) {
   // TODO return a promise using `fetch()`
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw new Error(`Network error: ${error.message}`);
+    });
 }
 
 function renderImage(data) {
   // TODO render the image to the DOM
+  const img = document.createElement('img');
+
+  img.src = data.img;
+  img.alt = data.alt;
+  document.body.appendChild(img);
+
   console.log(data);
 }
 
 function renderError(error) {
   // TODO render the error to the DOM
+  const h1 = document.createElement('h1');
+  h1.textContent = error;
+  document.body.appendChild(h1);
+
   console.log(error);
 }
 
 // TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);
