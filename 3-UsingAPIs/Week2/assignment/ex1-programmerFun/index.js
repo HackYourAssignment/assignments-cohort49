d5bba634bdd82d416f6b2f5fc0cccc0bcdd47ca0
Nima saghikalhori <nimasaghi.dev@gmail.com>
2024-09-20 18:19:51 +0200

    ex1-modified

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -18,17 +18,17 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 
-function requestData(url) {
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw new Error(`Network Error: ${error.message}`);
-    });
+async function requestData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error : ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Network error: ${error.message}`);
+  }
 }
 
 function renderImage(data) {
@@ -47,7 +47,7 @@ function renderError(error) {
 
 async function main() {
   try {
-    const data = await requestData('https://xkcd.now.shx/?comic=latest');
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
     renderImage(data);
   } catch (error) {
     renderError(error);
